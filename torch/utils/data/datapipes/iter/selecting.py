d60f8b8b91ed2c24de0c71fce0cbbfb305d8115f
ErjiaGuan <erjia@fb.com>
2021-02-08 11:50:01 -0500

    [DataLoader] Rename Callable to Map IterDataPipe
    ghstack-source-id: 3fe8590aa3f4c597a5162859667dea88d81be2b2
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/51879

--- torch/utils/data/datapipes/iter/selecting.py
+++ torch/utils/data/datapipes/iter/selecting.py
@@ -1,12 +1,12 @@
 from torch.utils.data import IterDataPipe
 from typing import Callable, TypeVar, Iterator
 
-from .callable import CallableIterDataPipe
+from .callable import MapIterDataPipe
 
 T_co = TypeVar('T_co', covariant=True)
 
 
-class FilterIterDataPipe(CallableIterDataPipe[T_co]):
+class FilterIterDataPipe(MapIterDataPipe[T_co]):
     r""" :class:`FilterIterDataPipe`.
 
     Iterable DataPipe to filter elements from datapipe according to filter_fn.
